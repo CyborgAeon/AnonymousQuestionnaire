93ee99ce089a0f5860d2f35ad0c9efcf0f469e64
Bard <38399164+CyborgAeon@users.noreply.github.com>
2023-07-31 16:02:43 +0100

    template for crud api, using sql connection

---
+++ app.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const sql = require("mssql");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+// Configuration for the database connection
+const dbConfig = {
+  port: 1433,
+  user: "NCLAN\\BRBAR",
+  password: "banana",
+  server: "localhost",
+  database: "Questionnaire",
+};
+
+// Endpoint for getting all surveys
+app.get("/surveys", async (req, res) => {
+  try {
+    const pool = await sql.connect(dbConfig);
+    const result = await pool.request().query("SELECT * FROM surveys");
+    res.json(result.recordset);
+  } catch (err) {
+    console.error("Error executing SQL query:", err);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+// Endpoint for creating a new survey
+app.post("/surveys", async (req, res) => {
+  const { title, description } = req.body;
+  if (!title) {
+    return res.status(400).json({ error: "Title is required" });
+  }
+
+  try {
+    const pool = await sql.connect(dbConfig);
+    const result = await pool
+      .request()
+      .input("title", sql.NVarChar, title)
+      .input("description", sql.NVarChar, description || null)
+      .query(
+        "INSERT INTO surveys (title, description) VALUES (@title, @description)"
+      );
+
+    res.json({
+      message: "Survey created successfully",
+      surveyId: result.recordset[0].surveyId,
+    });
+  } catch (err) {
+    console.error("Error executing SQL query:", err);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
+
+// Add other endpoints for updating and deleting surveys as needed.
+
+// Start the server
+app.listen(dbConfig, () => {
+  console.log(`Server running on http://${dbConfig.server}:${3000}`);
+});
